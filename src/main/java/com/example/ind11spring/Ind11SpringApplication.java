85639e48cb462fac91d010f0dd8d189537cfc0ef
Yelenasau <elenasauschkina@mail.ru>
2023-07-11 00:00:00 +0400

    1

---
+++ src/main/java/com/example/ind11spring/Ind11SpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.ind11spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Ind11SpringApplication {
+
+    public static void main(String[] args) {SpringApplication.run(Ind11SpringApplication.class, args);
+    }
+}
+
---
+++ src/test/java/com/example/ind11spring/Ind11SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ind11spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ind11SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
